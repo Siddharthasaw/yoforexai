fc0bc57183bfa07ef966f70aa508473a340bb4de
Aditya Shaw <adityashaw9066@gmail.com>
2025-07-04 19:02:44 +0530

    working on responsive

--- .next/types/app/(dashboard)/page.ts
+++ .next/types/app/(dashboard)/page.ts
@@ -1,4 +1,4 @@
-// File: /home/yoforexai-frontend/app/(dashboard)/page.tsx
+// File: C:\Users\aitra\Desktop\yoforexai\app\(dashboard)\page.tsx
 import * as entry from '../../../../app/(dashboard)/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
